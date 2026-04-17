6d6683527b560542f9340f1131a176df696d961e
RuneKyndal <rune@kyndal.dk>
2026-04-17 04:57:37 -0700

    Update and rename sw-timer.js to sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'heli-v1.24';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'sw.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});

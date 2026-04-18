16b3e651dce03f838f752fdbab277bc96840f1a5
RuneKyndal <rune@kyndal.dk>
2026-04-17 19:19:03 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,23 @@
-const CACHE_NAME = 'heli-v1.27';
+const CACHE_NAME = 'heli-v1.29';
 const ASSETS = [
   'index.html',
   'manifest.json',
   'sw.js'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  // Cleans up old caches (v1.26, etc) to save space on your phone
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-  self.clients.claim();
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
